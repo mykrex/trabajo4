cd9bc5bba70b9f33dc8a00c5887e33a453d108c6
MariaMartinez <marymmartynez@gmail.com>
2025-03-06 16:02:28 -0600

    Side bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,25 @@ export default function Home() {
   }
 
   return (
-    <div className="grid grid-columns-[20px_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <button onClick={fetchData} className="fixed top-4 right-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-        Fetch Data
-      </button>
-      {loading && <div style={{ textAlign: 'center', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>Loading...</div>}
+    <div>
+      <div className="fixed top-4 left-4">
+        <h1>History of People</h1>
+        <pre>{JSON.stringify(history,null, 2)}</pre>
+      </div>
 
-      <h1 className="text-xl">Current person</h1>
-      <pre>{JSON.stringify(currentPerson, null, 2)}</pre>
+      <main>
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+          <button onClick={fetchData} className="fixed top-4 right-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+            Fetch Data
+          </button>
+          {loading && <div style={{ textAlign: 'center', position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>Loading...</div>}
 
-      <h1>History of People</h1>
-      <pre>{JSON.stringify(history,null, 2)}</pre>
+          <div>
+            <h1 className="text-xl">Current person</h1>
+            <pre>{JSON.stringify(currentPerson, null, 2)}</pre>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
